7164956acb867cf64df57568753bc54d2ff7c16d
Dennis Cope <dennisaaroncope@gmail.com>
2018-12-30 03:07:46 -0700

    Changes:
    (1) Game is now played straight from the web browser, rather than from inside the browser console.
    
    (2) Altered comments for clarity.
    
    (2) Execution of game changed to be entirely dependent on the user input buttons.
    
    (3) Moved JavaScript code into its own file to reduce clutter.

--- README.md
+++ README.md
@@ -1,18 +1,14 @@
-README:
-This is a small JavaScript based game of rock, paper, scissors played through the browser console.
-
-To play, open the .html file in an internet browser, and open the browser console (CTRL + SHIFT + J) for interactivity.
+# README:
+This is a small JavaScript based game of rock, paper, scissors. To play, open the .html file in an internet browser.
 
 The rules: Rock beats scissors, scissors beats paper, and paper beats rock.
 
-The player is prompted for their input of rock, paper, scissors, which is subsequently compared against a random computer input.
+The player has a choice of selecting rock, paper, scissors for each round. Their choice is subsequently compared against a random computer input. The first player to reach a score of five, wins. To play again, refresh the page.
 
-Five rounds of rock, paper, scissors are played, and whoever has highest score at the end wins. To play again, refresh the page.
+This program was created as a small project to test the author's understanding of basic JavaScript functions, conditionals, and DOM manipulation.
 
-This program was created as a small project to test the author's understanding of basic JavaScript functions and conditionals.
-
-GITHUB REPOSITORY: 
+# GITHUB REPOSITORY: 
 https://github.com/coped/rock-paper-scissors
 
-AUTHOR: 
+# AUTHOR: 
 Dennis Cope, https://github.com/coped
---
+++ rock-paper-scissors.js
@@ -0,0 +1,115 @@
+let container = document.querySelector('#container');
+container.setAttribute('style', 'text-align: center');
+
+let playerScore = 0;
+let computerScore = 0;
+let resultMsg = '';
+let round = 1;
+
+
+let playDiv = document.querySelector('.play');
+
+// Creation of computer input
+function computerPlay() {
+    let inputValue = Math.random();
+    
+    if (inputValue < (1/3)) {
+        return 'rock';
+    } else if (inputValue > (2/3)) {
+        return 'paper';
+    } else { 
+        return 'scissors';
+    }
+}
+
+// Logic for round execution
+function playRound(playerSelection, computerSelection) {
+    // In case of tie.
+    if (playerSelection === computerSelection) {
+        resultMsg = 'Tie!';
+        return 'tie';
+    }
+    // If player chooses rock
+    else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        resultMsg = 'Win! Rock beats scissors.';
+        return 'win';
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        resultMsg = 'Loss! Rock loses to paper.';
+        return 'loss';
+    }
+    // If player chooses paper
+    else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        resultMsg = 'Win! Paper beats rock.';
+        return 'win';
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        resultMsg = 'Loss! Paper loses to scissors.';
+        return 'loss';
+    }
+    // If player chooses scissors
+    else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        resultMsg = 'Win! Scissors beat paper.';
+        return 'win';
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        resultMsg = 'Loss! Scissors loses to rock.';
+        return 'loss';
+    }
+}
+// Execution based on user input
+let playRoundResult;
+let rockBtn = document.querySelector('#rock');
+rockBtn.addEventListener('click', () => {
+    playRoundResult = playRound('rock', computerPlay());
+    displayRound();
+    gameScore();
+    displayResult();
+});
+let paperBtn = document.querySelector('#paper');
+paperBtn.addEventListener('click', () => {
+    playRoundResult = playRound('paper', computerPlay());
+    displayRound();
+    gameScore();
+    displayResult();
+});
+let scissorsBtn = document.querySelector('#scissors');
+scissorsBtn.addEventListener('click', () => {
+    playRoundResult = playRound('scissors', computerPlay());
+    displayRound();
+    gameScore();
+    displayResult();
+});
+
+// Display of round result
+function displayRound() {
+    let roundResult = document.createElement('h3')
+    roundResult.textContent = resultMsg;
+    playDiv.appendChild(roundResult);
+    round++;
+}
+
+// Adding and displaying game score
+let score = document.createElement('h2');
+score.textContent = 'Score: 0 - 0';
+playDiv.appendChild(score);
+function gameScore() {
+    if (playRoundResult === 'win') {
+        playerScore++;
+        score.textContent = 'Score: ' + playerScore + ' - ' + computerScore;       
+    } else if (playRoundResult === 'loss') {
+        computerScore++;
+        score.textContent = 'Score: ' + playerScore + ' - ' + computerScore;
+    } else if (playRoundResult === 'tie') {
+        score.textContent = 'Score: ' + playerScore + ' - ' + computerScore;
+    } else {}
+}
+
+// End result messages
+let endResultDiv = document.querySelector('.end-results');
+let endResult = document.createElement('h1');
+function displayResult() {
+    if (playerScore === 5) {
+        endResult.textContent = 'PLAYER WINS WITH A FINAL SCORE OF ' + playerScore + ' - ' + computerScore;
+    } else if (computerScore === 5) {
+        endResult.textContent = 'OPPONENT WINS WITH A FINAL SCORE OF ' + playerScore + ' - ' + computerScore;
+    } 
+    endResultDiv.appendChild(endResult);
+}
